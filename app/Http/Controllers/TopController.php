af0928b0f18d87fd6fb02ecde016f9fe2312ab4a
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2023-11-11 20:01:13 +0900

    add top index

---
+++ app/Http/Controllers/TopController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Database\QueryException;
+use Illuminate\Http\Request;
+
+class TopController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+
+        // POST送信がある時のみ、検索処理を実行
+        if($request->isMethod('post')) {
+            try {
+                $posts = Post::with(['user', 'category'])->paginate(10);
+            } catch(QueryException $e) {
+                $error = $e->getMessage();
+            }
+        } else {
+            $posts = [];
+        }
+
+        return view('top.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}

bed76300619849790391760ce063c1fcd08da067
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2023-11-11 17:52:19 +0900

    add make relation between models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
@@ -12,4 +13,14 @@ class Post extends Model
     use SoftDeletes;
 
     protected $guarded = [];
+
+    public function user() : BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category() : BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }

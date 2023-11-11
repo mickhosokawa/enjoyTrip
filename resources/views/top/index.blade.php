917bdc53df450e7fcf8618f81fd01e7e89315396
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2023-11-11 18:21:58 +0900

    add searched result section

--- resources/views/top/index.blade.php
+++ resources/views/top/index.blade.php
@@ -23,6 +23,7 @@
   <!-- 検索セクション -->
   <section class="container mx-auto mt-12 p-8 bg-white rounded-lg shadow-lg max-w-2xl">
     <form class="space-y-4" method="POST" action="{{ route('top.index') }}">
+      @csrf
       <div>
         <input type="text" placeholder="Let's explore your destination." class="w-full px-4 py-2 border rounded-lg" />
       </div>
@@ -36,6 +37,22 @@
     </form>
   </section>
 
+  <!-- 検索結果表示セクション  -->
+  <section>
+    @if($posts)
+    @foreach ($posts as $post)
+      <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mt-5">
+        <div class="md:flex">
+          <div class="p-8">
+            <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{{ $post->user->name }}</div>
+            <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{{ $post->title }}</a>
+            <p class="mt-2 text-gray-500">{{ Str::limit($post->body, 100) }}</p>
+          </div>
+        </div>
+      </div>
+    @endforeach
+    @endif
+  </section>
   <!-- プロモーションセクション -->
   <section class="container mx-auto mt-8">
     <!-- プロモーションコンテンツ -->

504d0710af3c0cf4a1e0ca6c589c6b0f545edcb9
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2023-11-11 20:21:56 +0900

    add display posting page

---
+++ resources/views/post/create.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Local Tour</title>
+  <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <h1>Ask locals!</h1>
+</body>
+</html>

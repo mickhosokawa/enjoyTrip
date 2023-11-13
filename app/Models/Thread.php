76666f212462c73d1150f5e65cf9e0e6c9a8bf52
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2023-11-13 17:40:32 +0900

    :add relationship for reply

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Thread extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    // Thread が属する User
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // Thread が属する Post
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
 }

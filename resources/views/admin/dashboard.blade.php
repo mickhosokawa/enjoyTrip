39068a810ad5294a8dab8d998160b6b6e9e7f303
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2023-11-22 07:28:33 +0900

    add blade for admin login

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adminダッシュボード</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+</head>
+<body class="bg-gray-100 dark:bg-gray-900">
+
+    <header class="bg-white dark:bg-gray-800 shadow">
+        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                Adminのダッシュボード
+            </h2>
+        </div>
+    </header>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <h1 class="text-gray-800 dark:text-white">Adminのダッシュボード</h1>
+                    <p>You're logged in!</p>
+
+                    <div>{{ Auth::user()->name }}</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>

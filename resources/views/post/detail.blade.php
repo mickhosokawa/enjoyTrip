506894250c238a46d40844b4cff8d3dceb582033
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2023-11-26 06:59:56 +0900

    :update enable validation message

--- resources/views/post/detail.blade.php
+++ resources/views/post/detail.blade.php
@@ -30,7 +30,16 @@
         <form id="post-form" method="POST" action="{{ route('reply.store', ['id' => $id]) }}">
           @csrf
            <div class="w-full mb-4 border border-gray-200 rounded-lg bg-gray-50 dark:bg-gray-700 dark:border-gray-600">
-               <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
+              @if ($errors->any())
+                <div class="alert alert-danger text-red-500 ">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+              @endif
+              <div class="px-4 py-2 bg-white rounded-t-lg dark:bg-gray-800">
                    <label for="comment" class="sr-only">Your comment</label>
                    <textarea name="reply" id="comment" rows="4" class="w-full px-0 text-sm text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400" placeholder="Write a comment..." required></textarea>
                </div>

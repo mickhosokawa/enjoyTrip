d63698fc9054325fd30461411d9f5b32d3b28f53
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2023-11-25 17:22:22 +0900

    update authenticate user before store

--- app/Http/Controllers/ReplyController.php
+++ app/Http/Controllers/ReplyController.php
@@ -11,28 +11,29 @@ use Illuminate\Support\Facades\Auth;
 
 class ReplyController extends Controller
 {
-    protected $user;
-
-    public function __construct()
-    {
-        $this->user = Auth::user();
-    }
 
     /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request, string $id)
     {
-
+        
         try {
+
+            $user = Auth::user();
+            
+            if(!$user) {
+                throw new Exception('User is not logged in');
+            }
+
             $reply = Thread::create([
-                'user_id' => $this->user->id,
+                'user_id' => $user->id,
                 'post_id' => $id,
                 'reply' => $request->reply,
             ]);
 
             // return response()->json($reply, 201);
-            return redirect()->back();
+            return redirect()->back()->with(['success' => 'added reply']);
         
         } catch(Exception $e) {
             throw $e;

9627590e24579de237085a5e99d1918040480571
mickhosokawa <mikiya.hosokawa0831@gmail.com>
2023-11-25 14:02:08 +0900

    change method to get data

--- resources/views/top/index.blade.php
+++ resources/views/top/index.blade.php
@@ -24,10 +24,10 @@
 
   <!-- 検索セクション -->
   <section class="container mx-auto mt-12 p-8 bg-white rounded-lg shadow-lg max-w-2xl">
-    <form class="space-y-4" method="POST" action="{{ route('top.index') }}"> <!-- idを削除 -->
+    <form class="space-y-4" method="POST" id="post-form">
       @csrf
       <div>
-        <input type="text" name="destination" id="destination" placeholder="Let's explore your destination." class="w-full px-4 py-2 border rounded-lg" />
+        <input type="text" name="title" id="title" placeholder="Let's explore your destination." class="w-full px-4 py-2 border rounded-lg" />
       </div>
       {{-- <div class="flex space-x-4">
         <input type="date" class="w-full px-4 py-2 border rounded-lg" />
@@ -40,20 +40,7 @@
   </section>
 
   <!-- 検索結果表示セクション  -->
-  <section>
-    @if($posts)
-    @foreach ($posts as $post)
-      <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl mt-5">
-        <div class="md:flex">
-          <div class="p-8">
-            <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">{{ $post->user->name }}</div>
-            <a href="{{ route('post.detail', ['id' => $post->id]) }}" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{{ $post->title }}</a>
-            <p class="mt-2 text-gray-500">{{ Str::limit($post->body, 100) }}</p>
-          </div>
-        </div>
-      </div>
-    @endforeach
-    @endif
+  <section id="content">
   </section>
   <!-- プロモーションセクション -->
   <section class="container mx-auto mt-8">
